1c3e7dc0a58548274d8cfce9131cbee9e615c897
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-25 21:51:27 -0300

    criando servidor

---
+++ aula2.js
@@ -0,0 +1,8 @@
+// criando servidor
+const http = require('http')
+
+http.createServer( (request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+    response.write('Hello World')
+    response.end()
+}).listen(3000)
