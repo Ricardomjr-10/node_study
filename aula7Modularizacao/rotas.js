56d4e2229fe0e59d19244020c3da25e9b83b8838
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-27 09:20:02 -0300

    build: routers

---
+++ aula7Modularizacao/rotas.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const rotas = express.Router()
+
+let cursosInfo = [
+{'curso': 'Node','info': 'Curso de Node'},
+{'curso': 'React','info': 'Curso de React'},
+{'curso': 'Angular','info': 'Curso de Angular'},
+{'curso': 'Vue','info': 'Curso de Vue'}
+]
