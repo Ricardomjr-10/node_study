f6d9d79b9af61436d6cbb5bf06867246fe121066
Ricardo-10 <ricardomjr2@gmail.com>
2024-11-27 09:26:35 -0300

    build: function rotas.get

--- aula7Modularizacao/rotas.js
+++ aula7Modularizacao/rotas.js
@@ -7,3 +7,19 @@ let cursosInfo = [
 {'curso': 'Angular','info': 'Curso de Angular'},
 {'curso': 'Vue','info': 'Curso de Vue'}
 ]
+
+rotas.get('/', (req, res) => {
+    res.json({ola: 'Seja bem vindo!'})
+})
+
+rotas.get('/:cursoid', (req, res) => {
+    const curso = req.params.cursoid
+    const cursoInfo = cursosInfo.find(i => i.curso === curso)
+    if (!cursoInfo) {
+        res.status(404).json({
+            erro: 'Curso nao encontrado', cursoPesquisado: curso
+        })
+    }else {
+        res.status(200).json(cursoInfo)
+    }
+})
