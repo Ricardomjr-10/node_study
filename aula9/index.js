da56ead00a6959d2dada5d05b130a8bf8b8ab89b
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-02 09:38:36 -0300

    servidor rodando

--- aula9/index.js
+++ aula9/index.js
@@ -1,6 +1,12 @@
 // criando servidor
 const http = require('http')
 const porta = process.env.PORT || 3000
+const retorno = () => console.log(`Servidor rodando na porta ${porta}`)
 
 const server = http.createServer((req, res) => {
-    
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.write('CBF cursos')
+    res.end()
+})
+
+server.listen(porta, retorno)
