0c7be7beb5778232e99a6117444e1fe63563799e
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-01 21:53:10 -0300

    inserindo dados pela variavel

--- aula8BdMysqlp1/index.js
+++ aula8BdMysqlp1/index.js
@@ -1,7 +1,9 @@
 (async () => {
     const db = require('./db')
     console.log('Inserir novo cliente')
-    await db.insereCliente({nome: 'Ricardo', idade: 40})
+    const nom = 'Fulano'
+    const ida = 20
+    await db.insereCliente({nome: nom, idade: ida})
     console.log('Obter todos os clientes')
     const clientes = await db.todosClientes()
     console.log(clientes)
