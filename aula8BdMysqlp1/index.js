54a553308ba11e442bbb5638fb3cee1584f0ad90
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-01 21:50:11 -0300

    inserindo novo cliente

--- aula8BdMysqlp1/index.js
+++ aula8BdMysqlp1/index.js
@@ -1,5 +1,7 @@
 (async () => {
     const db = require('./db')
+    console.log('Inserir novo cliente')
+    await db.insereCliente({nome: 'Ricardo', idade: 40})
     console.log('Obter todos os clientes')
     const clientes = await db.todosClientes()
     console.log(clientes)
