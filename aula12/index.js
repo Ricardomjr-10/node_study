ffbbc4cc5d345eed2dc01b8a9f5540b408910160
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-02 10:01:58 -0300

    criando constantes do servidor

---
+++ aula12/index.js
@@ -0,0 +1,7 @@
+//upload de arquivos com o modulo formidable
+
+const http = require('http');
+const port = process.env.PORT || 3000;
+const formidable = require('formidable')
+const fs = require('fs')
+
