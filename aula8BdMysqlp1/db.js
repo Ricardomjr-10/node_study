3f9ab24844897a9f938e4abb551ac76290a0b74a
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-01 21:35:10 -0300

    selecionado banco de dados

--- aula8BdMysqlp1/db.js
+++ aula8BdMysqlp1/db.js
@@ -3,13 +3,17 @@ const conectar = async () => {
     if (global.conexao && global.conexao.state !== 'disconnected')
          return global.conexao
     const mysql = require('mysql2/promise')
-    const connection =  mysql.createConnection("mysql://root:1234@localhost:3306/aula8")
+    const connection =  mysql.createConnection("mysql://dba:ricardo123@localhost:3306/cbfcursos")
     console.log('Conectado com sucesso!')
     global.conexao = connection
     return connection
 
 }
 
-conectar()
+const todosClientes = async () => {
+    const connection = await conectar()
+    const [rows] = await connection.query('SELECT * FROM cliente_node')
+    return await rows
+}
 
 module.exports = {}
