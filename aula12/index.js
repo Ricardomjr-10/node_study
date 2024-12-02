7044af7a9b0c7c626f2612a25a7d10b9cc158c74
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-02 10:08:58 -0300

    servidor pronto

--- aula12/index.js
+++ aula12/index.js
@@ -5,3 +5,13 @@ const port = process.env.PORT || 3000;
 const formidable = require('formidable')
 const fs = require('fs')
 
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">')
+    res.write('<input type="file" name="filetoupload"><br>')
+    res.write('<input type="submit" value="upload">')
+    res.write('</form>')
+    return res.end()
+})
+
+server.listen(port)
