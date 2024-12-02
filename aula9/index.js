260dde47907c94ab808ab42573f192f9625789b6
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-02 09:53:25 -0300

    rodando eventos

--- aula9/index.js
+++ aula9/index.js
@@ -1,12 +1,23 @@
 // criando servidor
 const http = require('http')
 //criando eventos
+const eventos = require('events')
+
+const EventEmitter = new eventos.EventEmitter()
+
+const final =() => {console.log('fim do processo')}
+
+EventEmitter.on('msg', () => {console.log('evento disparado')})
+EventEmitter.on('fim', final)
+
 
 const porta = process.env.PORT || 3000
 const retorno = () => console.log(`Servidor rodando na porta ${porta}`)
 const server = http.createServer((req, res) => {
+    EventEmitter.emit('msg')
     res.writeHead(200, {'Content-Type': 'text/plain'})
     res.write('CBF cursos')
+    EventEmitter.emit('fim')
     res.end()
 })
 
