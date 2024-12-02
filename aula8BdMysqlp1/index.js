3a95f5a4d163d09df8a4d2438cb350ff16f2b240
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-01 22:08:43 -0300

    atualizando idade de fulano

--- aula8BdMysqlp1/index.js
+++ aula8BdMysqlp1/index.js
@@ -1,10 +1,19 @@
 (async () => {
     const db = require('./db')
-    console.log('Inserir novo cliente')
+
+    // console.log('Inserir novo cliente')
+    // const nom = 'Fulano'
+    // const ida = 20
+    // await db.insereCliente({nome: nom, idade: ida})
+
+    const id = 3
     const nom = 'Fulano'
-    const ida = 20
-    await db.insereCliente({nome: nom, idade: ida})
+    const ida = 25
+    await db.atualizaCliente(id,{nome: nom, idade: ida})
+    console.log('cliente' + id + ' atualizado')
+
     
+
     console.log('Obter todos os clientes')
     const clientes = await db.todosClientes()
     console.log(clientes)
