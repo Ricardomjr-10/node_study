4f9bfa1f2a04147d89035288a31b3685a41e0876
Ricardo-10 <ricardomjr2@gmail.com>
2024-12-02 09:42:40 -0300

    evento msg criado

--- aula9/index.js
+++ aula9/index.js
@@ -1,8 +1,9 @@
 // criando servidor
 const http = require('http')
+//criando eventos
+
 const porta = process.env.PORT || 3000
 const retorno = () => console.log(`Servidor rodando na porta ${porta}`)
-
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'})
     res.write('CBF cursos')
